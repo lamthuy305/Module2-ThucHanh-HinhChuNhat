f46b769ffe9e50feb2344f1aab25098f66828191
lamthuy305 <lamthuy305@gmail.com>
2022-01-06 22:00:51 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,17 @@
+package com.codegym;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width: ");
+        int width = scanner.nextInt();
+        System.out.print("Enter the height: ");
+        int height = scanner.nextInt();
+
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+}
---
+++ src/com/codegym/Rectangle.java
@@ -0,0 +1,25 @@
+package com.codegym;
+
+public class Rectangle {
+    int width, height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getArea() {
+        return this.width * this.height;
+    }
+
+    public int getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
